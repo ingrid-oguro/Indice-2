2cdb826c028f787eeea327ce9e480be07c4eb8dc
ingrid-oguro <104454935+ingrid-oguro@users.noreply.github.com>
2023-03-02 09:56:28 -0300

    Update Periodo_Academico(Turma_e_Graduacao).py

--- Periodo_Academico(Turma_e_Graduacao).py
+++ Periodo_Academico(Turma_e_Graduacao).py
@@ -3,14 +3,14 @@ import numpy as np
 import streamlit as st
 import altair as alt
 
-#import pip
-#pip.main(["install", "openpyxl"])
+import pip
+pip.main(["install", "openpyxl"])
 PAGE_CONFIG = {"page_title": "Indicadores", "page_icon": ":globe_with_meridians:", "layout": "wide"}
 st.set_page_config(**PAGE_CONFIG)
 
-base = pd.read_excel('/content/drive/MyDrive/Relatorios_20231/base/periodo_graduação.xlsx')
-base_turma = pd.read_excel('/content/drive/MyDrive/Relatorios_20231/base/periodo_turma.xlsx')
-periodo = pd.read_excel('/content/drive/MyDrive/Relatorios_20231/base/periodo.xlsx')
+base = pd.read_excel('base/periodo_graduação.xlsx')
+base_turma = pd.read_excel('base/periodo_turma.xlsx')
+periodo = pd.read_excel('base/periodo.xlsx')
 #
 
 cursos_grad = ['Administração', 'Ciências Econômicas','Direito','Engenharia de Produção','Engenharia de Computação']
@@ -64,9 +64,6 @@ st.altair_chart(b, use_container_width=True)
 
 with st.expander("Ver base"):
     st.dataframe(df2)
-
-
-
 
 
 
